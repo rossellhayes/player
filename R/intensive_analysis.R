285250ae25e7a21892f8748a366abf0ccb453a1e
rossellhayes <alexander@rossellhayes.com>
2025-08-29 13:50:21 -0400

    feat(intensive_analysis): assign result into global env

--- R/intensive_analysis.R
+++ R/intensive_analysis.R
@@ -1,4 +1,4 @@
-intensive_analysis <- function(minutes = 5, speed = 1, background_job = TRUE) {
+intensive_analysis <- function(minutes = 0.8, speed = 2, lambda = 1, background_job = TRUE) {
   cli::cli_text("What is your question?")
   question <- input("Question: ", transform = identity)
 
@@ -7,10 +7,17 @@ intensive_analysis <- function(minutes = 5, speed = 1, background_job = TRUE) {
   look_busy(
     minutes,
     speed,
+    lambda,
     background_job,
-    end = {
+    end = cli::cli_alert_success("Results are ready!")
+  )
+
+  delayedAssign(
+    "intensive_analysis_results",
+    {
       cli::cli_h1("Question: {question}")
       magic_8_ball()
-    }
+    },
+    assign.env = .GlobalEnv
   )
 }
